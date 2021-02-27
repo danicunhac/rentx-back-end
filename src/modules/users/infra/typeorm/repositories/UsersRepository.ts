89f94da8b819952d4ac33e1182b0f86a263aeb93
Daniel Cunha <danielcc.jp@gmail.com>
2021-02-27 19:45:15 -0300

    feat: users

---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, Repository } from 'typeorm';
+import CreateUserDTO from '../../../dtos/CreateUserDTO';
+import IUsersRepository from '../../../repositories/IUsersRepository';
+import User from '../entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository(User);
+  }
+
+  async findById(id: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne(id);
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne({ where: { email } });
+
+    return user;
+  }
+
+  public async create(userData: CreateUserDTO): Promise<User> {
+    const user = this.ormRepository.create(userData);
+
+    await this.ormRepository.save(user);
+
+    return user;
+  }
+
+  save(user: User): Promise<User> {
+    return this.ormRepository.save(user);
+  }
+}
+
+export default UsersRepository;
